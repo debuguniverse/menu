cc9579cfc52d12c933cb05728a5ac88339bc0aae
debuguniverse <2516155956@qq.com>
2018-05-10 10:38:59 +0800

    menu init project

---
+++ Menu.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mloong
+ * Date: 2018/5/10
+ * Time: 10:33
+ */
+
+class Menu
+{
+
+    public function add(){
+
+    }
+
+    public function create(){
+
+    }
+
+    public function getMenuTree(){
+
+    }
+
+    public function getMenuList(){
+
+    }
+
+
+}
